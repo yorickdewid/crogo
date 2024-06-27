e1fe8b5acf67757c7387a4aad21e5a9b2b0a0a24
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-06-27 16:36:27 +0200

    Add Glonax message classes for serialization and deserialization

--- glonax/client.py
+++ glonax/client.py
@@ -1,30 +1,16 @@
+import datetime
 import socket
 import struct
 import time
 import uuid
 import logging
-from random import randbytes
 from enum import Enum
 from time import sleep
+from typing import Any, Callable
+from random import randbytes
+
 
 logger = logging.getLogger(__name__)
-
-
-class StatusType(Enum):
-    HEALTHY = 0xF8
-    DEGRADED = 0xF9
-    FAULTY = 0xFA
-    EMERGENCY = 0xFB
-
-    def __str__(self):
-        if self == StatusType.HEALTHY:
-            return "healthy"
-        elif self == StatusType.DEGRADED:
-            return "degraded"
-        elif self == StatusType.FAULTY:
-            return "faulty"
-        elif self == StatusType.EMERGENCY:
-            return "emergency"
 
 
 class MachineType(Enum):
@@ -119,126 +105,6 @@ class Request(Packet):
         return Request(MessageType(data[0]))
 
 
-class ModuleStatus(Packet):
-    def __init__(self, name, state, error_code):
-        self.name = name
-        self.state = state
-        self.error_code = error_code
-
-    def to_bytes(self):
-        return struct.pack("B", self.type.value)
-
-    def from_bytes(data):
-        name_length = struct.unpack(">H", data[0:2])[0]
-        name = data[2 : 2 + name_length].decode("utf-8")
-
-        state = data[2 + name_length]
-        error_code = data[3 + name_length]
-
-        return ModuleStatus(name, state, error_code)
-
-    def __str__(self):
-        return f"Name: {self.name}, State: {self.state}, Error code: {self.error_code}"
-
-
-class Instance(Packet):
-    def __init__(self, id, model, machine_type, version, serial_number):
-        self.id = id
-        self.model = model
-        self.machine_type = machine_type
-        self.version = version
-        self.serial_number = serial_number
-
-    def to_bytes(self):
-        data = self.id.bytes
-        data += struct.pack("B", self.machine_type.value)
-        data += struct.pack("BBB", *self.version)
-        data += struct.pack(">H", len(self.model))
-        data += self.model.encode("utf-8")
-
-        return data
-
-    def from_bytes(data):
-        id = uuid.UUID(bytes=data[:16])
-        machine_type = MachineType(data[16])
-        version = struct.unpack("BBB", data[17:20])
-
-        model_length = struct.unpack(">H", data[20:22])[0]
-        model = data[22 : 22 + model_length].decode("utf-8")
-
-        serial_number_length = struct.unpack(
-            ">H", data[22 + model_length : 24 + model_length]
-        )[0]
-        serial_number = data[
-            24 + model_length : 24 + model_length + serial_number_length
-        ].decode("utf-8")
-
-        return Instance(id, model, machine_type, version, serial_number)
-
-    def __str__(self):
-        return f"ID: {self.id}, Model: {self.model}, Type: {self.machine_type}, Version: {self.version[0]}.{self.version[1]}.{self.version[2]}, Serial number: {self.serial_number}"
-
-
-class VMS(Packet):
-    def __init__(self, memory, swap, cpu_load, uptime, timestamp):
-        self.memory = memory
-        self.swap = swap
-        self.cpu_load = cpu_load
-        self.uptime = uptime
-        self.timestamp = timestamp
-
-    def to_bytes(self):
-        data = struct.pack(">QQ", *self.memory)
-        data += struct.pack(">QQ", *self.swap)
-        data += struct.pack(">ddd", *self.cpu_load)
-        data += struct.pack(">Q", self.uptime)
-        data += struct.pack(">q", self.timestamp)
-
-        return data
-
-    def from_bytes(data):
-        memory = struct.unpack(">QQ", data[:16])
-        swap = struct.unpack(">QQ", data[16:32])
-        cpu_load = struct.unpack(">ddd", data[32:56])
-        uptime = struct.unpack(">Q", data[56:64])[0]
-        timestamp = struct.unpack(">q", data[64:72])[0]
-
-        return VMS(memory, swap, cpu_load, uptime, timestamp)
-
-    def __str__(self):
-        memory_used = self.memory[0] / 1024 / 1024 / 1024
-        memory_total = self.memory[1] / 1024 / 1024 / 1024
-        swap_used = self.swap[0] / 1024 / 1024 / 1024
-        swap_total = self.swap[1] / 1024 / 1024 / 1024
-        return f"Memory usage: {memory_used:.2f}GB / {memory_total:.2f}GB, Swap usage: {swap_used:.2f}GB / {swap_total:.2f}GB, CPU load: {self.cpu_load[0]:.1f}% {self.cpu_load[1]:.1f}%, {self.cpu_load[2]:.1f}%, Uptime: {self.uptime}s, Timestamp: {self.timestamp}"
-
-
-class Engine(Packet):
-    def __init__(self, driver_demand, actual_engine, rpm):
-        self.driver_demand = driver_demand
-        self.actual_engine = actual_engine
-        self.rpm = rpm
-        # self.status = status
-
-    def to_bytes(self):
-        data = struct.pack("BB", self.driver_demand, self.actual_engine)
-        data += struct.pack(">H", self.rpm)
-        data += struct.pack("B", self.status.value)
-
-        return data
-
-    def from_bytes(data):
-        driver_demand = data[0]
-        actual_engine = data[1]
-        rpm = struct.unpack(">H", data[2:4])[0]
-        # status = EngineStatus(data[4])
-
-        return Engine(driver_demand, actual_engine, rpm)
-
-    def __str__(self):
-        return f"Driver demand: {self.driver_demand}%, Actual engine: {self.actual_engine}%, RPM: {self.rpm}"
-
-
 class Control(Packet):
     class ControlType(Enum):
         ENGINE_REQUEST = 0x01
@@ -288,85 +154,17 @@ class Control(Packet):
         return Control(type, value)
 
 
-class Gnss(Packet):
-    def __init__(self, location, altitude, speed, heading, satellites):
-        self.location = location
-        self.altitude = altitude
-        self.speed = speed
-        self.heading = heading
-        self.satellites = satellites
-        # self.status = status
+class TcpConnection:
+    def __init__(
+        self,
+        address: str = "localhost",
+        port: str | int = 30051,
+        on_connect: Callable[[Any], None] | None = None,
+    ):
+        self.server_ip = address
+        self.server_port = port
 
-    def to_bytes(self):
-        data = struct.pack("ff", *self.location)
-        data += struct.pack("ff", self.altitude, self.speed, self.heading)
-        data += struct.pack("B", self.satellites)
-        # data += struct.pack("BB", self.satellites, self.status.value)
-
-        return data
-
-    def from_bytes(data):
-        location = struct.unpack("ff", data[:8])
-        altitude, speed, heading = struct.unpack("fff", data[8:20])
-        satellites = struct.unpack("B", data[20:21])
-
-        return Gnss(location, altitude, speed, heading, satellites[0])
-
-    def __str__(self):
-        return f"Location: {self.location}, Altitude: {self.altitude}, Speed: {self.speed}, Heading: {self.heading}, Satellites: {self.satellites}"
-
-
-class Frame:
-    def parse(data):
-        if data[:3] != b"LXR":
-            print("Invalid header received")
-            return
-
-        if data[3:4] != b"\x02":
-            print("Invalid protocol version")
-            return
-
-        message_type = MessageType(struct.unpack("B", data[4:5])[0])
-        message_length = struct.unpack(">H", data[5:7])[0]
-
-        if data[7:10] != b"\x00\x00\x00":
-            print("Invalid message checksum")
-            return
-
-        return message_type, message_length
-
-    def parse_message(data, message_type):
-        if message_type == MessageType.STATUS:
-            status_value = struct.unpack("B", data[:1])[0]
-            status = Status(status_value)
-            return status
-
-        elif message_type == MessageType.VMS:
-            memory = struct.unpack(">QQ", data[:16])
-            swap = struct.unpack(">QQ", data[16:32])
-            cpu_load = struct.unpack(">ddd", data[32:56])
-            uptime = struct.unpack(">Q", data[56:64])
-            timestamp = struct.unpack(">q", data[64:72])
-
-            return memory, swap, cpu_load, uptime, timestamp
-
-    def _header(type, length):
-        header = b"LXR\x02"
-        header += struct.pack("B", type.value)
-        header += struct.pack(">H", length)
-        header += b"\x00\x00\x00"
-        return header
-
-    def status(status):
-        header = Frame._header(MessageType.STATUS, 1)
-        return header + struct.pack("B", status.value)
-
-
-class Client:
-    def __init__(self, server_ip, server_port, user_agent="pyglonax/0.2"):
-        self.server_ip = server_ip
-        self.server_port = server_port
-        self.user_agent = user_agent
+        self.on_connect = on_connect
 
     def connect(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -377,7 +175,8 @@ class Client:
 
         logger.debug("Connected to the Glonax server")
 
-        self._handshake()
+        if self.on_connect:
+            self.on_connect()
 
     def send(self, type, data):
         header = b"LXR\x03"
@@ -388,7 +187,7 @@ class Client:
 
         self.sock.sendall(header + data)
 
-    def recv(self):
+    def recv(self) -> tuple[MessageType, bytes]:
         header = self.sock.recv(10)
 
         if header[:3] != b"LXR":
@@ -414,18 +213,83 @@ class Client:
 
         return message_type, message
 
-    def _probe(self):
+
+from glonax.message import Instance
+
+
+APPLICATION_TYPES = [
+    MessageType.STATUS,
+    MessageType.MOTION,
+    MessageType.VMS,
+    MessageType.GNSS,
+    MessageType.ENGINE,
+    MessageType.TARGET,
+    MessageType.CONTROL,
+    MessageType.ROTATOR,
+]
+
+
+class GlonaxClient:
+    def __init__(
+        self,
+        address: str = "localhost",
+        port: str | int = 30051,
+        user_agent: str = "pyglonax/0.2",
+        on_connect: Callable[[Any], None] | None = None,
+        on_reconnect: Callable[[Any], None] | None = None,
+        on_message: Callable[[Any, MessageType, bytes], None] | None = None,
+        on_error: Callable[[Any, Any], None] | None = None,
+        on_close: Callable[[Any, Any], None] | None = None,
+    ):
+        self.server_ip = address
+        self.server_port = port
+        self.user_agent = user_agent
+
+        self.on_connect = on_connect
+        self.on_reconnect = on_reconnect
+        self.on_message = on_message
+        self.on_error = on_error
+        self.on_close = on_close
+
+        self.sock = socket
+
+        self.conn = TcpConnection(
+            address=address,
+            port=port,
+            on_connect=self._on_connect,
+        )
+        self.conn.connect()
+
+    def _on_connect(self):
+        latency = self.probe()
+        logger.debug(f"Conection latency: {latency:.2f} seconds")
+
+        self._handshake()
+
+        if self.on_connect:
+            self.on_connect(self)
+
+    def ping(self) -> float:
         """
         Sends an echo message to the server and measures the elapsed time for the response.
 
         Returns:
-            float: The elapsed time in seconds.
+            float: The elapsed time in milliseconds.
+        """
+        return self.probe()
+
+    def probe(self) -> float:
+        """
+        Sends an echo message to the server and measures the elapsed time for the response.
+
+        Returns:
+            float: The elapsed time in milliseconds.
         """
         snd_echo = Echo()
         start_time = time.time()
-        self.send(MessageType.ECHO, snd_echo.to_bytes())
+        self.conn.send(MessageType.ECHO, snd_echo.to_bytes())
 
-        message_type, message = self.recv()
+        message_type, message = self.conn.recv()
         end_time = time.time()
         if message_type == MessageType.ECHO:
             rcv_echo = Echo.from_bytes(message)
@@ -445,12 +309,10 @@ class Client:
         Raises:
             SomeException: If an error occurs during the handshake process.
         """
-        self._probe()
-
         session = Session(self.user_agent)
-        self.send(MessageType.SESSION, session.to_bytes())
+        self.conn.send(MessageType.SESSION, session.to_bytes())
 
-        message_type, message = self.recv()
+        message_type, message = self.conn.recv()
         if message_type == MessageType.INSTANCE:
             self.machine = Instance.from_bytes(message)
 
@@ -501,7 +363,7 @@ class Client:
         Args:
             value (bool): The value to set the machine horn.
         """
-        self.send(
+        self.conn.send(
             MessageType.CONTROL,
             Control(Control.ControlType.MACHINE_HORN, value).to_bytes(),
         )
@@ -513,7 +375,7 @@ class Client:
         Args:
             value (bool): The value to set the machine lights.
         """
-        self.send(
+        self.conn.send(
             MessageType.CONTROL,
             Control(Control.ControlType.MACHINE_LIGHTS, value).to_bytes(),
         )
@@ -525,31 +387,34 @@ class Client:
         Args:
             value (bool): The value to set for the machine illumination. True to turn on the illumination, False to turn it off.
         """
-        self.send(
+        self.conn.send(
             MessageType.CONTROL,
             Control(Control.ControlType.MACHINE_ILLUMINATION, value).to_bytes(),
         )
 
-    def shutdown(self):
-        self.send(
-            MessageType.CONTROL,
-            Control(Control.ControlType.MACHINE_SHUTDOWN, 0).to_bytes(),
-        )
-
     def hydraulic_lock(self, value: bool):
-        self.send(
+        self.conn.send(
             MessageType.CONTROL,
             Control(Control.ControlType.HYDRAULIC_LOCK, value).to_bytes(),
         )
 
     def hydraulic_quick_disconnect(self, value: bool):
-        self.send(
+        self.conn.send(
             MessageType.CONTROL,
             Control(Control.ControlType.HYDRAULIC_QUICK_DISCONNECT, value).to_bytes(),
         )
 
+    # TODO: Implementaton is invalid
     def engine_request(self, value: int):
-        self.send(
+        self.conn.send(
             MessageType.CONTROL,
             Control(Control.ControlType.ENGINE_REQUEST, value).to_bytes(),
         )
+
+    def listen(self):
+        while True:
+            message_type, message = self.conn.recv()
+
+            if message_type in APPLICATION_TYPES:
+                if self.on_message:
+                    self.on_message(self, message_type, message)
---
+++ glonax/message.py
@@ -0,0 +1,171 @@
+import datetime
+import struct
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class Instance(BaseModel):
+    id: UUID
+    model: str
+    machine_type: int
+    version: tuple[int, int, int]
+    serial_number: str
+
+    def from_bytes(data):
+        id = UUID(bytes=data[:16])
+        machine_type = data[16]
+        version = struct.unpack("BBB", data[17:20])
+
+        model_length = struct.unpack(">H", data[20:22])[0]
+        model = data[22 : 22 + model_length].decode("utf-8")
+
+        serial_number_length = struct.unpack(
+            ">H", data[22 + model_length : 24 + model_length]
+        )[0]
+        serial_number = data[
+            24 + model_length : 24 + model_length + serial_number_length
+        ].decode("utf-8")
+
+        return Instance(
+            id=id,
+            model=model,
+            machine_type=machine_type,
+            version=version,
+            serial_number=serial_number,
+        )
+
+    def to_bytes(self):
+        return (
+            self.id.bytes
+            + struct.pack("B", self.machine_type)
+            + struct.pack("BBB", *self.version)
+            + struct.pack(">H", len(self.model))
+            + self.model.encode("utf-8")
+            + struct.pack(">H", len(self.serial_number))
+            + self.serial_number.encode("utf-8")
+        )
+
+
+class ModuleStatus(BaseModel):
+    name: str
+    state: int
+    error_code: int
+
+    def from_bytes(data):
+        name_length = struct.unpack(">H", data[0:2])[0]
+        name = data[2 : 2 + name_length].decode("utf-8")
+
+        state = data[2 + name_length]
+        error_code = data[3 + name_length]
+
+        return ModuleStatus(name=name, state=state, error_code=error_code)
+
+    def to_bytes(self):
+        return (
+            struct.pack(">H", len(self.name))
+            + self.name.encode("utf-8")
+            + struct.pack("BB", self.state, self.error_code)
+        )
+
+
+class VMS(BaseModel):
+    memory_used: int
+    memory_total: int  # TODO: Remove, should not change
+    swap_used: int  # TODO: Remove, swap should always be 0
+    swap_total: int  # TODO: Remove, swap should always be 0
+    cpu_load: list[float]
+    uptime: int
+    timestamp: datetime.datetime
+
+    @property
+    def memory_used_mb(self):
+        return self.memory_used / 1024 / 1024
+
+    @property
+    def memory_total_mb(self):
+        return self.memory_total / 1024 / 1024
+
+    @property
+    def swap_used_mb(self):
+        return self.swap_used / 1024 / 1024
+
+    @property
+    def swap_total_mb(self):
+        return self.swap_total / 1024 / 1024
+
+    def from_bytes(data):
+        memory_used = struct.unpack(">Q", data[:8])[0]
+        memory_total = struct.unpack(">Q", data[8:16])[0]
+        swap_used = struct.unpack(">Q", data[16:24])[0]
+        swap_total = struct.unpack(">Q", data[24:32])[0]
+        cpu_load = struct.unpack(">ddd", data[32:56])
+        uptime = struct.unpack(">Q", data[56:64])[0]
+        timestamp = struct.unpack(">q", data[64:72])[0]
+
+        return VMS(
+            memory_used=memory_used,
+            memory_total=memory_total,
+            swap_used=swap_used,
+            swap_total=swap_total,
+            cpu_load=cpu_load,
+            uptime=uptime,
+            timestamp=datetime.datetime.fromtimestamp(timestamp),
+        )
+
+    def to_bytes(self):
+        return (
+            struct.pack(">Q", self.memory_used)
+            + struct.pack(">Q", self.memory_total)
+            + struct.pack(">Q", self.swap_used)
+            + struct.pack(">Q", self.swap_total)
+            + struct.pack(">ddd", *self.cpu_load)
+            + struct.pack(">Q", self.uptime)
+            + struct.pack(">q", self.timestamp.timestamp())
+        )
+
+
+class Engine(BaseModel):
+    driver_demand: int
+    actual_engine: int
+    rpm: int = Field(default=0, ge=0, le=8000)
+
+    def from_bytes(data):
+        driver_demand = data[0]
+        actual_engine = data[1]
+        rpm = struct.unpack(">H", data[2:4])[0]
+
+        return Engine(driver_demand=driver_demand, actual_engine=actual_engine, rpm=rpm)
+
+    def to_bytes(self):
+        return bytes([self.driver_demand, self.actual_engine]) + struct.pack(
+            ">H", self.rpm
+        )
+
+
+class Gnss(BaseModel):
+    location: tuple[float, float]
+    altitude: float
+    speed: float
+    heading: float
+    satellites: int
+
+    def from_bytes(data):
+        location = struct.unpack("ff", data[:8])
+        altitude, speed, heading = struct.unpack("fff", data[8:20])
+        satellites = struct.unpack("B", data[20:21])
+
+        return Gnss(
+            location=location,
+            altitude=altitude,
+            speed=speed,
+            heading=heading,
+            satellites=satellites[0],
+        )
+
+    def to_bytes(self):
+        return (
+            struct.pack("ff", *self.location)
+            + struct.pack("fff", self.altitude, self.speed, self.heading)
+            + struct.pack("B", self.satellites)
+        )
